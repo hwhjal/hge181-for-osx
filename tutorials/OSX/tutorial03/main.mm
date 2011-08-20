6a9c7553aaab602cdcddba854181cc35de50faf3
a.onofreytchuk <A.Onofreytchuk@gmail.com>
2011-08-20 08:43:34 +0000

    Project Release configuration changed

--- tutorials/OSX/tutorial03/main.mm
+++ tutorials/OSX/tutorial03/main.mm
@@ -14,8 +14,11 @@
 #include "../../../include/hgefont.h"
 #include "../../../include/hgeparticle.h"
 
-HGE *hge = 0;
-GLclampf c = 0.0f;	
+
+// Pointer to the HGE interface.
+// Helper classes require this to work.
+HGE *hge=0;
+
 
 // Pointers to the HGE objects we will use
 hgeSprite*			spr;
@@ -27,14 +30,6 @@ hgeParticleSystem*	par;
 HTEXTURE			tex;
 HEFFECT				snd;
 
-int scrDx = 800;
-int scrDy = 600;
-const bool bWnd = true;
-//int scrDx = 1280;
-//int scrDy = 1024;
-//bool bWnd = false;
-
-
 // Some "gameplay" variables
 float x=100.0f, y=100.0f;
 float dx=0.0f, dy=0.0f;
@@ -42,50 +37,29 @@ float dx=0.0f, dy=0.0f;
 const float speed=190;
 const float friction=0.98f;
 
-// This function plays collision sound with
-// parameters based on sprite position and speed
+// Play sound effect
 void boom() {
 	int pan=int((x-400)/4);
 	float pitch=(dx*dx+dy*dy)*0.0005f+0.2f;
 	hge->Effect_PlayEx(snd,100,pan,pitch);
 }
 
-
 bool FrameFunc()
 {
-	float dt=hge->Timer_GetDelta();	
+	float dt=hge->Timer_GetDelta();
 	
 	// Process keys
 	if (hge->Input_GetKeyState(HGEK_ESCAPE)) return true;
-	if (hge->Input_GetKeyState(HGEK_LEFT))	dx-=speed*dt;
+	if (hge->Input_GetKeyState(HGEK_LEFT)) dx-=speed*dt;
 	if (hge->Input_GetKeyState(HGEK_RIGHT)) dx+=speed*dt;
 	if (hge->Input_GetKeyState(HGEK_UP)) dy-=speed*dt;
 	if (hge->Input_GetKeyState(HGEK_DOWN)) dy+=speed*dt;
 	
-	if(hge->Input_GetKeyState(HGEK_W))
-	{
-		scrDx = 1280;
-		scrDy = 1024;		
-		hge->System_SetState(HGE_SCREENWIDTH, 1280);
-		hge->System_SetState(HGE_SCREENHEIGHT, 1024);		
-		hge->System_SetState(HGE_WINDOWED, false);
-	}
-	if(hge->Input_GetKeyState(HGEK_Q))
-	{ 
-		scrDx = 800;
-		scrDy = 600;
-		hge->System_SetState(HGE_SCREENWIDTH, 800);
-		hge->System_SetState(HGE_SCREENHEIGHT, 600);		
-		hge->System_SetState(HGE_WINDOWED, true);
-	}
-	
-	
-	
 	// Do some movement calculations and collision detection	
 	dx*=friction; dy*=friction; x+=dx; y+=dy;
-	if(x>scrDx) {x=scrDx-(x-scrDx);dx=-dx;boom();}
+	if(x>784) {x=784-(x-784);dx=-dx;boom();}
 	if(x<16) {x=16+16-x;dx=-dx;boom();}
-	if(y>scrDy) {y=scrDy-(y-scrDy);dy=-dy;boom();}
+	if(y>584) {y=584-(y-584);dy=-dy;boom();}
 	if(y<16) {y=16+16-y;dy=-dy;boom();}
 	
 	// Update particle system
@@ -97,9 +71,6 @@ bool FrameFunc()
 }
 
 
-// This function will be called by HGE when
-// the application window should be redrawn.
-// Put your rendering code here.
 bool RenderFunc()
 {
 	// Render graphics
@@ -116,36 +87,32 @@ bool RenderFunc()
 
 int main (int argc, char * const argv[])
 {
-	hge = hgeCreate (HGE_VERSION);
+	hge = hgeCreate(HGE_VERSION);
 	
-	// Set up log file, frame function, render function and window title
-	hge->System_SetState(HGE_LOGFILE, "hge_tut02.log");
+	hge->System_SetState(HGE_LOGFILE, "hge_tut03.log");
 	hge->System_SetState(HGE_FRAMEFUNC, FrameFunc);
 	hge->System_SetState(HGE_RENDERFUNC, RenderFunc);
-	hge->System_SetState(HGE_TITLE, "HGE Tutorial 02 - Using input, sound and rendering");
-	hge->System_SetState(HGE_FPS, 60);
-	
-	// Set up video mode 
-	hge->System_SetState(HGE_WINDOWED, bWnd);
-	hge->System_SetState(HGE_ZBUFFER, true);
-	hge->System_SetState(HGE_SCREENWIDTH, scrDx);
-	hge->System_SetState(HGE_SCREENHEIGHT, scrDy);
+	hge->System_SetState(HGE_TITLE, "HGE Tutorial 03 - Using helper classes");
+	hge->System_SetState(HGE_FPS, 100);
+	hge->System_SetState(HGE_WINDOWED, true);
+	hge->System_SetState(HGE_SCREENWIDTH, 800);
+	hge->System_SetState(HGE_SCREENHEIGHT, 600);
 	hge->System_SetState(HGE_SCREENBPP, 32);
 	
-	if(hge->System_Initiate())
-	{
+	if(hge->System_Initiate()) {
+		
+		// Load sound and texture
 		snd=hge->Effect_Load("menu.wav");
 		tex=hge->Texture_Load("particles.png");
 		if(!snd || !tex)
 		{
 			// If one of the data files is not found, display
 			// an error message and shutdown.
-			// MessageBox(NULL, "Can't load MENU.WAV or PARTICLES.PNG", "Error", MB_OK | MB_ICONERROR | MB_APPLMODAL);
+			// MessageBox(NULL, "Can't load one of the following files:\nMENU.WAV, PARTICLES.PNG, FONT1.FNT, FONT1.PNG, TRAIL.PSI", "Error", MB_OK | MB_ICONERROR | MB_APPLMODAL);
 			hge->System_Shutdown();
 			hge->Release();
 			return 0;
 		}
-		
 		
 		// Create and set up a sprite
 		spr=new hgeSprite(tex, 96, 64, 32, 32);
@@ -170,8 +137,12 @@ int main (int argc, char * const argv[])
 		delete fnt;
 		delete spt;
 		delete spr;
-		hge->Texture_Free(tex);		
-	}	
+		hge->Texture_Free(tex);
+		hge->Effect_Free(snd);
+	}
 	
-    return 0;
+	// Clean up and shutdown
+	hge->System_Shutdown();
+	hge->Release();
+	return 0;
 }
